df5d90eeea8636c98c901771e049be5ef8ddd988
Grant Fitzgerald <gfitzgerald8@gatech.edu>
2025-06-22 21:59:46 -0500

    Updating formatting of navigation bar for all pages and spacing of rules in rules.html

--- app/app.py
+++ app/app.py
@@ -198,7 +198,9 @@ def view_rules():
             rules = f.read()
     except Exception as e:
         rules = f"Error reading rules file: {e}"
-    return render_template("rules.html", rules=rules)
+    return render_template("rules.html", 
+                           rules=rules,
+                           active_page="rules")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
